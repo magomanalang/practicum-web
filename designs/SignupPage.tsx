f48fb396aa87562537e857e5630b9fa21b582448
MatYu <manalangmatthew13@gmail.com>
2026-05-06 13:47:48 +0800

    working

--- designs/SignupPage.tsx
+++ designs/SignupPage.tsx
@@ -15,7 +15,7 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
