6176ee4ef1dd2770457a7090630616a46e854428
MatYu <manalangmatthew13@gmail.com>
2026-06-26 15:44:10 +0800

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@base-ui/react";
 import { LoanProductsTable } from "../_components/tables/LoanProductsTable";
 
 export default function Page() {
-  return <LoanProductsTable />;
+  return (
+    <>
+      <Button>Create Product for Approval</Button>
+      <LoanProductsTable />
+    </>
+  );
 }
