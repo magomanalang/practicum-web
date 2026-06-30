545ecec7ff3e75d0199559c608984a8941619423
MatYu <manalangmatthew13@gmail.com>
2026-06-30 23:08:36 +0800

    Update route.ts

--- app/api/auth/add-phone-details/route.ts
+++ app/api/auth/add-phone-details/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
 
-    const backendUrl = `${process.env.API_URL}/api/Employee/register-employee`;
+    const backendUrl = `${process.env.API_URL}/api/PhoneDetail/add-phone-details`;
 
     const dotNetResponse = await fetch(backendUrl, {
       method: "POST",
